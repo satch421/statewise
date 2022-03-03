1d10fd092682598833c22ca4d8d58b2e12d86a1f
Pete Godston <56722508+satch421@users.noreply.github.com>
2022-03-03 08:49:33 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_1dBzudKbY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7f8fcetghdn78t50on5ir6ipvl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' 
+    },
+    api: {
+        invokeUrl: 'https://6mf51nqbji.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
